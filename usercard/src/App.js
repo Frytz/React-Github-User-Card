94c02432a707aa2104345758674f0be0f43774e3
John Rossi <DACOOK1983@gamil.com>
2020-05-12 21:18:53 -0400

    complete

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Axios from "axios";
+import { Header} from './components/Header';
 
 import UserCard from './components/UserCard';
 
@@ -8,8 +9,10 @@ export default class App extends React.Component {
 constructor(){
   super();
   this.state = {
+    userName: "frytz",
         user: [],
-        followers:[]
+        followers:[],
+        following:[],
   }
 }
 
@@ -18,23 +21,49 @@ componentDidMount(){
 }
 
 fetchData = () => {
-  Axios("https://api.github.com/users/Frytz")
+  Axios('https://api.github.com/users/' + this.state.userName)
   .then(res => {
     console.log(res.data)
     this.setState({...this.state, user: res.data})
+    
     console.log(this.state)
   })
   .catch(err => {
     console.log('err', err)
   })
 }
+handleLoginChange =  e => {
+this.setState({
+  userName: e.target.value
+})
+};
+
+handleLoginUpdate = e => {
+Axios("https://api.github.com/users/" + this.state.userName).then((res) => {
+  console.log(res.data);
+  this.setState({ ...this.state, user: res.data });
+
+  console.log(this.state);
+});
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-logo">Github User Card with React</h1>
+          <Header user={this.state.user}>
+           
+          </Header> 
+          <div>
+            <input type="text" placeholder="Login Name" value={this.state.UserName}  onChange= {this.handleLoginChange}/>
+          <button onClick ={this.handleLoginUpdate}>test</button>
+          </div>
         </header>
-        <UserCard />
+
+        <UserCard
+          user={this.state.user}
+          follows={this.state.user.following}
+          follow={this.state.user.followers}
+        />
       </div>
     );
   }
